f120919bcd2ffe3eebd3c65c8f69432a0c2e3812
Mohammad Belal Hossain <62785546+belalahmed143@users.noreply.github.com>
2023-06-19 20:46:53 +0600

    f

---
+++ class12/ef11_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Banner,Category, Product
+# Register your models here.
+
+admin.site.register(Banner)
+admin.site.register(Category)
+admin.site.register(Product)
---
+++ class12/ef11_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',home, name='home'),
+    path('about',about, name='about'),
+]
